336d9c62d5214d2f67e061b9928ffaee12a097f9
Raman-954 <152240479+Raman-954@users.noreply.github.com>
2025-12-29 12:26:40 +0530

    Implement removeZeros function in Solution class

---
+++ 3726LC.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long removeZeros(long long n) {
+      string s=to_string(n);
+      string s1="";
+      for(int i=0;i<s.size();i++){
+        if(s[i]!='0'){
+            s1+=s[i];
+        }
+      }
+      return stoll(s1);
+    }
+};
