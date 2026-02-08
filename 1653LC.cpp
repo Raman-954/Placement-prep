a9522b448c77e65a3fa2a9d8228fae22b46604d7
Raman-954 <152240479+Raman-954@users.noreply.github.com>
2026-02-08 22:14:14 +0530

    Implement minimumDeletions function in Solution class

---
+++ 1653LC.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int bCount = 0;
+        int deletions = 0;
+        for(char ch : s) {
+            if(ch == 'b') {
+                bCount++;
+            } else { 
+                deletions = min(deletions + 1, bCount);
+            }
+        }
+        return deletions;
+    }
+};
