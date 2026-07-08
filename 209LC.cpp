e3162a60e31569f6047c54aa9cd3d5c01684d367
Raman-954 <152240479+Raman-954@users.noreply.github.com>
2026-07-08 16:43:26 +0530

    Implement minSubArrayLen function in Solution class

---
+++ 209LC.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+     int n = nums.size();
+       int mini = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+          for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum >= target) {
+                mini = min(mini, j - i + 1);
+                    break;
+                }
+            }
+        }
+        return (mini == INT_MAX) ? 0 : mini;
+    }
+};
